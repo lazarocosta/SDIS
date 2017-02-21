cce20836e265668d5ded675835f3c1f3c3403252
José Aleixo <josealeixo.pc@gmail.com>
2017-02-21 12:04:47 +0000

    Added lab01 java files

---
+++ labs/l01/UDPClient.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.net.*;
+
+class UDPClient
+{   
+    public static void main(String args[]) throws Exception
+    {
+        if(args.length != 4 && args.length != 5)
+        {
+            System.out.println("Usage: java UDPClient <host_name> <port_number> <oper> <opnd>*");
+            return;
+        }
+
+        String hostName = args[0];      // generally "localhost"
+        String portNumber = args[1];    
+        String operation = args[2];     // "register" or "lookup"
+    
+        String message;
+
+        switch(operation)
+        {
+            case "register":
+            {
+                if(args.length == 4)
+                {
+                    System.out.println("There is missing an argument for 'register' function.");
+                    return;
+                }
+
+                String plateNumber = args[3];
+                String ownerName = args[4];
+                
+                message = "REGISTER " + plateNumber + " " + ownerName;
+            }
+
+            break;
+
+            case "lookup":
+            {
+                if(args.length == 5)
+                {
+                    System.out.println("There is an extra argument for 'lookup' function.");
+                    return;
+                }
+
+                String plateNumber = args[3];
+
+                message = "LOOKUP " + plateNumber;
+            }
+
+            break;
+
+            default:
+                System.out.println("Bad argument for 'operation.'");
+                return;
+        }
+
+        System.out.println("EXECUTING: " + message);
+
+        byte[] sendData = message.getBytes();   // byte array to store information to send data
+
+        // The socket constrcutor needs no port, because the port information is already in the packet
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        // Get IP adress by name resolution
+        InetAddress IPAddress = InetAddress.getByName(hostName);
+
+        // Send packet
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Integer.parseInt(portNumber));
+        clientSocket.send(sendPacket);
+
+        // Receive future packet
+        byte[] receiveData = new byte[4096];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        System.out.println("Size: " + receiveData.length);
+        String receivedMessage = new String(receivePacket.getData());
+        System.out.println("FROM SERVER: " + receivedMessage);
+        
+        clientSocket.close();
+    }
+}
---
+++ labs/l01/UDPServer.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+import java.util.HashMap;
+
+class UDPServer
+{
+
+    private Map <String, String> vehicles = new HashMap<String, String>();  // Map is an interface of HashMap, so a HashMap will have more
+
+    private void addVehicle(String numberPlate, String ownerName)
+    {
+        this.vehicles.put(numberPlate, ownerName);  // numberPlate should be the key, since it is unique. A person may have more than 1 car.
+}
+
+    private String lookupVehicleOwner(String numberPlate)
+    {
+        numberPlate = numberPlate.trim();
+        System.out.println(this.vehicles);
+        System.out.println(numberPlate);
+        
+        String owner = this.vehicles.get(numberPlate);
+        System.out.println(owner);
+
+        return owner;    
+    }
+
+    public String interpretRequest(String request)
+    {
+        String[] splitRequest = request.split("\\s+");  // regex 
+        String response;
+
+        switch(splitRequest[0])
+        {
+            case "REGISTER":
+            {
+                addVehicle(splitRequest[1], splitRequest[2]);
+                response = vehicles.size() + "\n" + splitRequest[1] + " " + splitRequest[2];
+            }
+
+            break;
+            
+            case "LOOKUP":
+            {
+                response = vehicles.size() + "\n" + splitRequest[1] + " " + lookupVehicleOwner(splitRequest[1]);
+            }
+
+            break;
+
+            default:
+                response = "-1";
+        }
+
+        return response;
+    }
+
+    public void run(String args[]) throws Exception
+    {
+           /** args[] is an array containing the arguments given.
+         *  If no argument is given, then args is empty.
+         * 
+         *  For example: java UDPServer arg0 arg1
+         */
+
+        if(args.length != 1)
+        {
+            System.out.println("Usage: java UPDServer <port_number>");
+            return;
+        }
+
+        int portGiven = Integer.parseInt(args[0]); 
+
+        // Creating a new Datagram Socket on the given port
+        DatagramSocket serverSocket = new DatagramSocket(portGiven);
+
+        // Server is always running
+        while(true)
+        {
+            // A byte array is used to create a Datagram Packet
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String request = new String(receivePacket.getData());
+            System.out.println("RECEIVED: " + request);
+
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+
+            String response = interpretRequest(request);
+            System.out.println("RESPONDING: " +  response);
+            sendData = response.getBytes();
+
+            System.out.println("Size: " +  sendData.length);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+    
+
+    public static void main(String args[]) throws Exception
+    {
+        try
+        {
+            UDPServer myServer = new UDPServer ();
+            myServer.run(args);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace ();
+        }
+    }
+}
