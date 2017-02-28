67af6d316600d0dfcd81200ac2880e5a9a6172c7
lazarocosta <lazaroosta@hotmail.com>
2017-02-28 23:03:40 +0000

    update

--- labs/l02/Client.java
+++ labs/l02/Client.java
@@ -18,8 +18,8 @@ public class Client {
 
 		String message;
 
-		switch (operation) {
-		case "register": {
+		switch (operation){
+		 case "register": {
 			if (args.length == 4) {
 				System.out.println("There is missing an argument for 'register' function.");
 				return;
@@ -29,24 +29,21 @@ public class Client {
 			String ownerName = args[4];
 
 			message = "REGISTER " + plateNumber + " " + ownerName;
-		}
-
+		 }
 			break;
 
-		case "lookup": {
+		 case "lookup": {
 			if (args.length == 5) {
 				System.out.println("There is an extra argument for 'lookup' function.");
 				return;
 			}
 
 			String plateNumber = args[3];
-
 			message = "LOOKUP " + plateNumber;
-		}
-
+		 }
 			break;
 
-		default:
+		 default:
 			System.out.println("Bad argument for 'operation.'");
 			return;
 		}
@@ -64,41 +61,30 @@ public class Client {
 		try (MulticastSocket clientSocket = new MulticastSocket(this.multicastPort)) {
 
 			//Joint the Multicast group.
-
 			clientSocket.joinGroup(address);
 
 			while (true) {
-
 				// Receive the information and print it.
-
 				DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
-
 				clientSocket.receive(msgPacket);
-
 				String msg = new String(buf, 0, buf.length);
-
 				System.out.println("Socket 1 received msg: " + msg);
 
 			}
-
 		} catch (IOException ex) {
-			
+
 			ex.printStackTrace();
-			
 		}
 	}
 
 	public static void main(String[] args) throws UnknownHostException {
 
-		try
-        {
-            Client myClient = new Client();
-            myClient.run(args);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace ();
-        }
+		try {
+			Client myClient = new Client();
+			myClient.run(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
