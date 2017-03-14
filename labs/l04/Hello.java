af6e81941b96338461942841246dd5786a4091ab
José Aleixo <josealeixo.pc@gmail.com>
2017-03-14 12:53:11 +0000

    Added Project01 folder and lab04 (RMI) basic class

---
+++ labs/l04/Hello.java
@@ -0,0 +1,13 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * The 'Hello' interface allows objects to use the 'Remote' interface, which lets remote methods be invoked.
+ * 
+ * A remote method is a method that is called by one computer to make another computer do some task. Normally, it envolves passing objects between computers in a serialized way.
+ * 
+ */
+
+public interface Hello extends Remote {
+    String sayHello() throws RemoteException;
+}
---
+++ p01/README.md
@@ -0,0 +1,3 @@
+## Project 1 - Distrbuted Backup Service
+
+Specification: https://web.fe.up.pt/~pfs/aulas/sd2017/projs/proj1/proj1.html
