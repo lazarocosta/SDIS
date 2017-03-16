656e67d07f42debf60e351961bbd2b0370404177
José Aleixo <josealeixo.pc@gmail.com>
2017-03-16 11:21:08 +0000

    Updated script

--- p01/run.sh
+++ p01/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 xterm -hold -e java -cp production Systems.Client &
-xterm -hold -e java -cp production Protocols.Server
+xterm -hold -e java -cp production Systems.Server
 
 # -cp is a flag standing for Class Path
